feb32de3850e136d82ba0c6dbaf4ee4d1db3b0e4
dkh.alamri <163071583+IbrahimTaiz@users.noreply.github.com>
2025-08-13 10:18:59 +0300

    corect erorr
    corect erorr

--- welcom.php
+++ welcom.php
@@ -1,41 +1,25 @@
 <?php
-
 declare(strict_types=1);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = $_POST['name'] ?? 'Guest';
-    $color = $_POST['color'] ?? 'black';
-
-    header(
-        'Location: result.php?name=' . urlencode($name) . '&color=' . urlencode($color),
-        true,
-        303
-    );
-    exit;
-}
+$name = $_GET['name'] ?? 'guest';
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ibrahim Alamri Welcome Page </title>
 </head>
 <body>
-
-    <form action="index.php" method="POST">
-        <label for="name">user name:</label>
+ 
+    <p>
+        Welcome <?php echo htmlspecialchars($name); ?>
+    </p>
+    <form action="welcom.php" method="GET">
         <input type="text" id="name" name="name" placeholder="Enter your name:">
-        <label for="color">favorite color:</label>
-        <input type="color" id="color" name="color" value="#FFFFFF">
         <input type="submit" value="Send">
     </form>
-
-    <?php
-    // Display current time
-    echo date('H:i:s');
-    ?>
-
+  
 </body>
 </html>
